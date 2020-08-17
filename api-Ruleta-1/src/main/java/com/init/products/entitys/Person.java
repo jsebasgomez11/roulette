890e5684d299ef36c83722c6145c30405e6619b0
jsebasgomez11 <37191173+jsebasgomez11@users.noreply.github.com>
2020-08-17 15:56:06 -0500

    Add files via upload

---
+++ api-Ruleta-1/src/main/java/com/init/products/entitys/Person.java
@@ -0,0 +1,34 @@
+package com.init.products.entitys;
+
+public class Person {
+	
+	/**
+	 * person features
+	 */
+	private int id;
+	private String name;
+	private int bet;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBet() {
+		return bet;
+	}
+	public void setBet(int bet) {
+		this.bet = bet;
+	}
+	
+	
+
+}
